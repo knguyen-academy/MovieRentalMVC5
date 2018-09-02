fa57a4542de82c4b000faf8f86a9603ca84261c2
knguyen-academy <knguyen.academy@gmail.com>
2018-09-02 14:05:09 -0700

    Add project files.

---
+++ MovieRentalMVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieRentalMVC5.MvcApplication" Language="C#" %>
---
+++ MovieRentalMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieRentalMVC5.Startup))]
+namespace MovieRentalMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
