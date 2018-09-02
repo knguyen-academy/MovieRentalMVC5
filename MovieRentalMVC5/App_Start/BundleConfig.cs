a64d97755b86e4fed7da61e7474853bfd99551cf
knguyen-academy <knguyen.academy@gmail.com>
2018-09-02 14:45:59 -0700

    Change lumen bootstrap

--- MovieRentalMVC5/App_Start/BundleConfig.cs
+++ MovieRentalMVC5/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace MovieRentalMVC5
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
