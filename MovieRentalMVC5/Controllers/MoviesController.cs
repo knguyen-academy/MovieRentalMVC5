f6d23677163c88aca4d189cc1dcb387bcc0e90f1
knguyen-academy <knguyen.academy@gmail.com>
2018-09-02 15:02:43 -0700

    Add Edit , Index Action in MovieController for parameter ex

--- MovieRentalMVC5/Controllers/MoviesController.cs
+++ MovieRentalMVC5/Controllers/MoviesController.cs
@@ -10,11 +10,29 @@ namespace MovieRentalMVC5.Controllers
 {
     public class MoviesController : Controller
     {
+
         // GET: Movies/Random
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };    // need MovieRentalMVC5.Models lib
             return View(movie);
+        }
+
+        // EXAMPLE of Parameter using Content Action
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // EXAMPLE of passing parameter
+        // Movie?pageIndex=2&sortBy=TestName
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
