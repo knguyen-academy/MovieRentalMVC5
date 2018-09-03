ac2c5676f790761e9faf7b6b1a027d66c1822932
knguyen-academy <knguyen.academy@gmail.com>
2018-09-03 12:48:09 -0700

    Exercise 1 - Add Customer list View, Movie list View

---
+++ MovieRentalMVC5/Controllers/CustomersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRentalMVC5.Models;
+
+namespace MovieRentalMVC5.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // INDEX
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+
+
+        //Customers/Detail/id
+        //Explain:
+        //Basically you get the list of customers, then you use the method SingleOrDefault to get a single value or a default value 
+        //where you abreviate every object inside of the list, meaning c is the same as a customer 
+        //then you compare the id of the c object with the id argument you received in your action.
+        //SingleOrDefault => "Returns the only element of a sequence, or a default value if the sequence is empty; "
+        public ActionResult Details(int id)
+        {
+
+            var customers = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customers == null)
+                return HttpNotFound();
+
+            return View(customers);
+        }
+
+
+        // GetCustomers Function
+        // Return a list of customers
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+           {
+                new Customer {Id=1, Name ="John Smith"},
+                new Customer {Id=2, Name ="Mary Williams"}
+           };
+
+        }
+    }
+}
--- MovieRentalMVC5/Controllers/MoviesController.cs
+++ MovieRentalMVC5/Controllers/MoviesController.cs
@@ -11,7 +11,27 @@ namespace MovieRentalMVC5.Controllers
     public class MoviesController : Controller
     {
 
-        // EXAMPLE: Movies/Random
+
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+
+        }
+
+
+        //Get List of movie
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id=1, Name="Shrek" },
+                new Movie {Id=2, Name="Ironman"}
+            };
+        }
+
+
+        // VIEW MODEL EXAMPLE
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };    // need MovieRentalMVC5.Models lib
@@ -37,16 +57,16 @@ namespace MovieRentalMVC5.Controllers
             return Content("id=" + id);
         }
 
-        // EXAMPLE of passing parameter to url
-        // Movie?pageIndex=2&sortBy=TestName
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
+        //// EXAMPLE of passing parameter to url
+        //// Movie?pageIndex=2&sortBy=TestName
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    if (!pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (String.IsNullOrWhiteSpace(sortBy))
+        //        sortBy = "Name";
+        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //}
 
         // EXAMPLE of Custom Route: movies/released/{year}/{month}
         // change: add custom route using using Attribute 
