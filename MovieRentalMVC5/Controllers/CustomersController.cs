a68fc0240bd02b0c7d92860fd58075f31728969c
knguyen-academy <knguyen.academy@gmail.com>
2018-09-03 22:00:29 -0700

    Add Customer Dbcontext in Customer Controller

--- MovieRentalMVC5/Controllers/CustomersController.cs
+++ MovieRentalMVC5/Controllers/CustomersController.cs
@@ -9,42 +9,78 @@ namespace MovieRentalMVC5.Controllers
 {
     public class CustomersController : Controller
     {
-        // INDEX
+
+
+        ///////////// QUERY DATA By DbContex //////////////
+        private ApplicationDbContext _context;
+
+        //Constructor
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //Dipose
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
             return View(customers);
         }
 
-
-
-        //Customers/Detail/id
-        //Explain:
-        //Basically you get the list of customers, then you use the method SingleOrDefault to get a single value or a default value 
-        //where you abreviate every object inside of the list, meaning c is the same as a customer 
-        //then you compare the id of the c object with the id argument you received in your action.
-        //SingleOrDefault => "Returns the only element of a sequence, or a default value if the sequence is empty; "
         public ActionResult Details(int id)
         {
 
-            var customers = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customers = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customers == null)
                 return HttpNotFound();
 
             return View(customers);
         }
 
+        //****** Previous Example ******/////
+        ///////////// BEGIN QUERY DATA BY GetCustomer //////////
+        //// INDEX
+        //public ActionResult Index()
+        //{
+        //    var customers = GetCustomers();
+        //    return View(customers);
+        //}
 
-        // GetCustomers Function
-        // Return a list of customers
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-           {
-                new Customer {Id=1, Name ="John Smith"},
-                new Customer {Id=2, Name ="Mary Williams"}
-           };
 
-        }
+
+        ////Customers/Detail/id
+        ////Explain:
+        ////Basically you get the list of customers, then you use the method SingleOrDefault to get a single value or a default value 
+        ////where you abreviate every object inside of the list, meaning c is the same as a customer 
+        ////then you compare the id of the c object with the id argument you received in your action.
+        ////SingleOrDefault => "Returns the only element of a sequence, or a default value if the sequence is empty; "
+        //public ActionResult Details(int id)
+        //{
+
+        //    var customers = GetCustomers().SingleOrDefault(c => c.Id == id);
+        //    if (customers == null)
+        //        return HttpNotFound();
+
+        //    return View(customers);
+        //}
+
+
+        //// GetCustomers Function
+        //// Return a list of customers
+        //private IEnumerable<Customer> GetCustomers()
+        //{
+        //    return new List<Customer>
+        //   {
+        //        new Customer {Id=1, Name ="John Smith"},
+        //        new Customer {Id=2, Name ="Mary Williams"}
+        //   };
+
+        //}
+        ///////////// END QUERY DATA BY GetCustomer //////////
     }
 }
