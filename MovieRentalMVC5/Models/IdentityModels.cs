b72a44d50e08180558139280f6b8361b8eb047d2
knguyen-academy <knguyen.academy@gmail.com>
2018-09-05 21:52:37 -0700

    Add Genre Model and Modified Movie Model

--- MovieRentalMVC5/Models/IdentityModels.cs
+++ MovieRentalMVC5/Models/IdentityModels.cs
@@ -22,6 +22,7 @@ namespace MovieRentalMVC5.Models
     {
         //Add customers table in dbset
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Movie> Movies { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
