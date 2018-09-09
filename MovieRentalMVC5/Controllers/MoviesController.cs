0678046e8e4d01b9cb931e75f41d43687886cda6
knguyen-academy <knguyen.academy@gmail.com>
2018-09-09 12:17:02 -0700

    Solution 3 - Display list of movies and their details

--- MovieRentalMVC5/Controllers/MoviesController.cs
+++ MovieRentalMVC5/Controllers/MoviesController.cs
@@ -5,76 +5,105 @@ using System.Web;
 using System.Web.Mvc;
 using MovieRentalMVC5.Models;
 using MovieRentalMVC5.ViewModels;
+using System.Data.Entity; //need for Eager loading
 
 namespace MovieRentalMVC5.Controllers
 {
     public class MoviesController : Controller
     {
+        // Initialize Movie context (from DB)
+        private ApplicationDbContext _context;
+
+        //Constructor
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //Destructor
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
 
         public ActionResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(g => g.Genre).ToList();
             return View(movies);
-
         }
 
 
-        //Get List of movie
-        private IEnumerable<Movie> GetMovies()
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
-            {
-                new Movie {Id=1, Name="Shrek" },
-                new Movie {Id=2, Name="Ironman"}
-            };
+            var movies = _context.Movies.Include(g => g.Genre).SingleOrDefault(m => m.Id == id);
+            return View(movies);
         }
 
-
-        // VIEW MODEL EXAMPLE
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek!" };    // need MovieRentalMVC5.Models lib
-
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
-
-            var viewModel = new RandomMovieViewModel() // need  MovieRentalMVC5.ViewModels lib
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
-        }
-
-        // EXAMPLE of Parameter using Content Action
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
-
-        //// EXAMPLE of passing parameter to url
-        //// Movie?pageIndex=2&sortBy=TestName
-        //public ActionResult Index(int? pageIndex, string sortBy)
+        ////PREVIOUS EXAMPLEs
+        //public ActionResult Index()
         //{
-        //    if (!pageIndex.HasValue)
-        //        pageIndex = 1;
-        //    if (String.IsNullOrWhiteSpace(sortBy))
-        //        sortBy = "Name";
-        //    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        //    var movies = GetMovies();
+        //    return View(movies);
+
         //}
 
-        // EXAMPLE of Custom Route: movies/released/{year}/{month}
-        // change: add custom route using using Attribute 
-        // by add routes.MapMvcAttributeRoutes() in routeconfig.cs
-        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, byte month)
-        {
-            return Content(year + "/" + month);
-        }
+
+        ////Get List of movie
+        //private IEnumerable<Movie> GetMovies()
+        //{
+        //    return new List<Movie>
+        //    {
+        //        new Movie {Id=1, Name="Shrek" },
+        //        new Movie {Id=2, Name="Ironman"}
+        //    };
+        //}
+
+
+        //// VIEW MODEL EXAMPLE
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Shrek!" };    // need MovieRentalMVC5.Models lib
+
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer {Name = "Customer 1"},
+        //        new Customer {Name = "Customer 2"}
+        //    };
+
+        //    var viewModel = new RandomMovieViewModel() // need  MovieRentalMVC5.ViewModels lib
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
+
+        //    return View(viewModel);
+        //}
+
+        //// EXAMPLE of Parameter using Content Action
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id=" + id);
+        //}
+
+        ////// EXAMPLE of passing parameter to url
+        ////// Movie?pageIndex=2&sortBy=TestName
+        ////public ActionResult Index(int? pageIndex, string sortBy)
+        ////{
+        ////    if (!pageIndex.HasValue)
+        ////        pageIndex = 1;
+        ////    if (String.IsNullOrWhiteSpace(sortBy))
+        ////        sortBy = "Name";
+        ////    return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        ////}
+
+        //// EXAMPLE of Custom Route: movies/released/{year}/{month}
+        //// change: add custom route using using Attribute 
+        //// by add routes.MapMvcAttributeRoutes() in routeconfig.cs
+        //[Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        //public ActionResult ByReleaseDate(int year, byte month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
     }
 }
